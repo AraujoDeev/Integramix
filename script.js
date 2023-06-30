9e326027d0e7da06ee566201b77fa1a9ec18429f
Matheus Araujo <103580714+AraujoDeev@users.noreply.github.com>
2023-06-30 16:00:35 -0300

    Deixando responsivo

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const titleRegiao = document.getElementById('title-regioes')
 const regioes = document.getElementById('regioes')
 const listaRegioes = document.getElementById('listaRegioes')
 const regioesBottom = document.getElementById('regioes-bottom')
+const ulListaCidades = document.getElementById('ul-lista-cidades')
+const menuBurguer = document.getElementById('burguer')
+const menuBurguerContainer = document.getElementById('menu-burguer')
 
 const regiaoCentral = [
   'Aclimação',
@@ -14,7 +17,38 @@ const regiaoCentral = [
   'Liberdade',
   'República',
   'Sé',
+  'Aclimação',
+  'Brás',
+  'Consolação',
+  'Liberdade',
+  'República',
+  'Sé',
+  'Aclimação',
+  'Brás',
+  'Consolação',
+  'Liberdade',
+  'República',
 ]
+
+const regiaoTeste = [
+  'Pimentas',
+  '1234',
+  'dfdfdf',
+  'Libervdvdvdade',
+  'Repúbladvadvica',
+  'Sadvadvadvé',
+]
+
+const openMenu = () => {
+  menuBurguer.addEventListener('click', () => {
+    if (menuBurguerContainer.style.display === 'none') {
+      menuBurguerContainer.style.display = 'flex'
+    } else {
+      menuBurguerContainer.style.display = 'none'
+    }
+  })
+}
+openMenu()
 
 const toggleRegioesBottom = () => {
   regioesBottom.classList.toggle('bottom-regioes-show')
@@ -38,3 +72,45 @@ dropdownBtn.addEventListener('click', function (e) {
   e.stopPropagation()
   toggleDropdown()
 })
+
+// Get the regioes container element
+let regioesContainer = document.getElementById('ul-lista-regioes')
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (let i = 0; i < aRegioes.length; i++) {
+  aRegioes[i].addEventListener('click', function () {
+    let current = document.getElementsByClassName('ativo')
+    current[0].className = current[0].className.replace(' ativo', '')
+    this.className += ' ativo'
+    const cidades = cidadesMostradas(this.children[0].id)
+    renderizarCidades(cidades)
+  })
+}
+
+const cidadesMostradas = (regiao) => {
+  switch (regiao) {
+    case 'central':
+      return regiaoCentral
+    case 'norte':
+      return regiaoTeste
+    default:
+      return regiaoCentral
+  }
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild)
+  }
+}
+
+const renderizarCidades = (cidades = regiaoCentral) => {
+  // Remove all child nodes from list of cities before rendering new ones
+  removeAllChildNodes(ulListaCidades)
+  cidades.map((cidade) => {
+    let cidadeLi = document.createElement('li')
+    cidadeLi.innerText = cidade
+    ulListaCidades.appendChild(cidadeLi)
+  })
+}
+renderizarCidades()
