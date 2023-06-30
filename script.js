10b0951a7eeb00ffbf1591ad7e26dbcf1b0d989a
Matheus Araujo <103580714+AraujoDeev@users.noreply.github.com>
2023-06-30 10:59:11 -0300

    Toggle Regioes

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const dropdownBtn = document.getElementById('btn')
 const dropdownMenu = document.getElementsByClassName('dropdown')
+const navRegioes = document.getElementById('nav')
+const aRegioes = document.querySelectorAll('.a-regioes')
+const titleRegiao = document.getElementById('title-regioes')
+const regioes = document.getElementById('regioes')
+const listaRegioes = document.getElementById('listaRegioes')
+const regioesBottom = document.getElementById('regioes-bottom')
+
+const regiaoCentral = [
+  'Aclimação',
+  'Brás',
+  'Consolação',
+  'Liberdade',
+  'República',
+  'Sé',
+]
+
+const toggleRegioesBottom = () => {
+  regioesBottom.classList.toggle('bottom-regioes-show')
+}
+
+const toggleRegiao = () => {
+  regioes.classList.toggle('regioes-show')
+}
+
+titleRegiao.addEventListener('click', () => {
+  toggleRegioesBottom()
+  toggleRegiao()
+})
 
 const toggleDropdown = function () {
   console.log(dropdownMenu)
